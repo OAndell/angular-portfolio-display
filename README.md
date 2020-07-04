184f9b499a55a555c5d10d612a6299e88ce3e9ef
OAndell <Oscar@Andell.eu>
2020-07-04 16:08:21 +0200

    .

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 Proof of concept application for displaying dynamic cards. 
 
 Each card has a image, name, description, and link.
+
+Example:
+https://oandell.github.io/angular-portfolio-display/
--- src/app/components/main-grid/main-grid.component.ts
+++ src/app/components/main-grid/main-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener,  } from '@angular/core';
 import { Entry } from '../../models/Entry';
 import {EntryService} from '../../services/entry.service';
 import {
@@ -26,14 +26,37 @@ import {
     ])
   ]
 })
+
 export class MainGridComponent implements OnInit {
 
   entryList:Entry[];
+  listToShow:Entry[] = [];
+  height:number = 1000;
+  lastY = window.pageYOffset;
+  yOffset = 0;
 
   constructor(private entryService:EntryService) { }
 
   ngOnInit(): void {
     this.entryList = this.entryService.getEntries();
+    this.listToShow = this.entryList.slice(0,5);
+
+  }
+
+  @HostListener('window:scroll', ['$event']) onWindowScroll($event) {
+      //this.listToShow.pop();
+      //this.listToShow.push(this.entryList.pop());
+      let yOffsetDiff = this.lastY - window.pageYOffset;
+      console.log(yOffsetDiff)
+      if(yOffsetDiff < -100){
+        this.listToShow.push(this.entryList.pop());
+        this.height = this.height + 100;
+        this.lastY =  window.pageYOffset
+      }
+
+
+
+
   }
 
 }
